91b1c9b05e681fce14392c951dd8b552bf2eb792
sayrarodriguezz <55262980+sayrarodriguezz@users.noreply.github.com>
2021-10-23 23:24:56 -0500

    Update AlertController.swift

--- Rezy/Rezy/Model/ViewController/Components/AlertController.swift
+++ Rezy/Rezy/Model/ViewController/Components/AlertController.swift
@@ -1,7 +1,16 @@
-
+//sayra(:
 import UIKit
 
-//sayra
-/*
- create an extensison for alertcontroller
- */
+extension UIViewController {
+    func alert(_ title:String="",_ messsage:String=""){
+     
+        // title:title - presents title of message
+        //message:message - present message below title
+       // preferredStyle: .alert - shows the textboc in the middle of screen
+        let alert = UIAlertController(title: title, message: messsage, preferredStyle: .alert) 
+
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel)) //adds action to button {ok}
+
+        self.present(alert, animated: true, completion: nil) 	//shows alert 
+    }
+}
