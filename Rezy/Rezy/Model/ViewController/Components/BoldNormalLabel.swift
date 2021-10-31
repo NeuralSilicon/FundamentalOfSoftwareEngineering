2662b72a0389b7be03fe92e06862c616c52ee3ab
jhaelon <80273017+jhaelon@users.noreply.github.com>
2021-10-31 11:49:17 -0500

    Update BoldNormalLabel.swift
    Added attributes and definitions. should be good to go now

--- Rezy/Rezy/Model/ViewController/Components/BoldNormalLabel.swift
+++ Rezy/Rezy/Model/ViewController/Components/BoldNormalLabel.swift
@@ -1,13 +1,18 @@
 
 import UIKit
 
-//Jhaelon
-/*
-need a class for boldnormal text
- */
-
-
 final class BoldNormalLabel{
- 
+    static func boldNormalText(_ bold: String, _ bSize: CGFloat, _ normal: String,_ nSize:CGFloat) -> NSAttributedString?{
+        
+        let attrs = [NSAttributedString.Key.font : UIFont.custom(type: .AppleSDGothicNeo_Bold, size: bSize)]
+        let attributedString = NSMutableAttributedString(string:bold, attributes:attrs)
+
+        let attr = [NSAttributedString.Key.font : UIFont.custom(type: .AppleSDGothicNeo_Light, size: nSize)]
+        let normalString = NSMutableAttributedString(string:normal, attributes:attr)
+        attributedString.append(normalString)
+               
+       return attributedString
+    }
+    
     
 }
