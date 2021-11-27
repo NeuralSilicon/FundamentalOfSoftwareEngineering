17910a2091bcc225f54dffd34ffa2bc2c36548fe
jhaelon <80273017+jhaelon@users.noreply.github.com>
2021-11-26 21:19:50 -0600

    Finally fixed the password encrypt code

--- Rezy/Rezy/Model/User/PasswordEncryption.swift
+++ Rezy/Rezy/Model/User/PasswordEncryption.swift
@@ -2,13 +2,17 @@
 import UIKit
 import CryptoSwift
 
-//Jhaelon
-/*
-add encryotion and decryption function, using CryptoSwift API
- */
-
 
 class Encryption{
     
+    func aesEncrypt(KEY:String, IV:String, password:String) throws -> String {
+        let encrypted = try AES(key: KEY, iv: IV, padding: .pkcs7).encrypt([UInt8](password.data(using: .utf8)!))
+        return Data(encrypted).base64EncodedString()
+    }
 
+    func aesDecrypt(KEY:String, IV:String, password:String) throws -> String {
+        guard let data = Data(base64Encoded: password) else { return "" }
+        let decrypted = try AES(key: KEY, iv: IV, padding: .pkcs7).decrypt([UInt8](data))
+        return String(bytes: decrypted, encoding: .utf8) ?? password
+    }
 }
